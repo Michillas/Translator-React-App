ee29a8c43d87ad73b60976ea5c14c027f938f42b
Michillas <elrickimont@gmail.com>
2024-01-18 16:47:32 +0100

    TranslatorReactApp - Divided and made more sustainable

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import { Label } from "./components/ui/label"
 import { Button } from "./components/ui/button"
 
 import {useEffect, useState} from 'react'
-import { Value } from "@radix-ui/react-select"
+
+import translateText from './api/translator'
+
+import ThemeIcon from './assets/themeicon'
 
 function App() {
 
@@ -99,85 +102,6 @@ function App() {
       </div>
     </main>
   );
-}
-
-function ThemeIcon(props) {
-  if (document.querySelector('body').classList.contains('dark')) {
-    return (
-      <svg
-        {...props}
-        xmlns="http://www.w3.org/2000/svg"
-        width="24"
-        height="24"
-        viewBox="0 0 24 24"
-        fill="none"
-        stroke="currentColor"
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      >
-        <path d="M12 3a6 6 0 0 0 9 9 9 9 0 1 1-9-9Z" />
-      </svg>
-    )
-  } else {
-    return (
-      <svg
-        {...props}
-        xmlns="http://www.w3.org/2000/svg"
-        width="24"
-        height="24"
-        viewBox="0 0 24 24"
-        fill="none"
-        stroke="currentColor"
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      >
-        <circle cx="12" cy="12" r="4" />
-        <path d="M12 2v2" />
-        <path d="M12 20v2" />
-        <path d="m4.93 4.93 1.41 1.41" />
-        <path d="m17.66 17.66 1.41 1.41" />
-        <path d="M2 12h2" />
-        <path d="M20 12h2" />
-        <path d="m6.34 17.66-1.41 1.41" />
-        <path d="m19.07 4.93-1.41 1.41" />
-      </svg>
-    )
-  }
-}
-
-async function translateText(selectedValueInput, selectedValueOutput) {
-
-  const inputText = document.getElementById('input-text')
-  const outputText = document.getElementById('output-text')
-
-  const url = "https://text-translator2.p.rapidapi.com/translate";
-  const options = {
-    method: "POST",
-    headers: {
-      "content-type": "application/x-www-form-urlencoded",
-      "X-RapidAPI-Key": "ec37769452mshdd95dd180c5fa6ep17a417jsnf073bf53cf53",
-      "X-RapidAPI-Host": "text-translator2.p.rapidapi.com",
-    },
-    body: new URLSearchParams({
-      source_language: selectedValueInput,
-      target_language: selectedValueOutput,
-      text: inputText.value,
-    }),
-  };
-  const promise = fetch(url, options);
-  promise
-    .then((v1) => {
-      return v1.json();
-    })
-    .then((v2) => {
-      try {
-        outputText.value = v2.data.translatedText;
-      } catch {
-        outputText.placeholder = v2.message;
-      }
-    });
 }
 
 export default App;
---
+++ src/api/translator.js
@@ -0,0 +1,34 @@
+async function translateText(selectedValueInput, selectedValueOutput) {
+
+  const inputText = document.getElementById('input-text')
+  const outputText = document.getElementById('output-text')
+
+  const url = "https://text-translator2.p.rapidapi.com/translate";
+  const options = {
+    method: "POST",
+    headers: {
+      "content-type": "application/x-www-form-urlencoded",
+      "X-RapidAPI-Key": "ec37769452mshdd95dd180c5fa6ep17a417jsnf073bf53cf53",
+      "X-RapidAPI-Host": "text-translator2.p.rapidapi.com",
+    },
+    body: new URLSearchParams({
+      source_language: selectedValueInput,
+      target_language: selectedValueOutput,
+      text: inputText.value,
+    }),
+  };
+  const promise = fetch(url, options);
+  promise
+    .then((v1) => {
+      return v1.json();
+    })
+    .then((v2) => {
+      try {
+        outputText.value = v2.data.translatedText;
+      } catch {
+        outputText.placeholder = v2.message;
+      }
+    });
+}
+
+export default translateText
